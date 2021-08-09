828325d0bd4e8cf4d3f9d8d13521f1aa8eaeca91
Hardiansa <hardiansa@maleo.social>
2021-08-09 18:22:25 +0700

    [adjustment] make NavItem reusable

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,14 @@ import Link from "next/link";
 
 type Props = {};
 
+const NavItem = ({ title, url = "/" }: { title: string; url?: string }) => {
+  return (
+    <Link href={url} passHref>
+      <a className="text-center text-gray-900 font-body">{title}</a>
+    </Link>
+  );
+};
+
 const Navbar: React.FC<Props> = () => {
   return (
     <header className="absolute left-0 right-0 w-full py-4 top-5">
@@ -16,23 +24,11 @@ const Navbar: React.FC<Props> = () => {
             </Link>
           </div>
           <nav className="hidden lg:flex lg:gap-x-8 lg:items-center">
-            <Link href="/" passHref>
-              <a className="text-center text-gray-900 font-body">
-                Desitnations
-              </a>
-            </Link>
-            <Link href="/" passHref>
-              <a className="text-center text-gray-900 font-body">Hotels</a>
-            </Link>
-            <Link href="/" passHref>
-              <a className="text-center text-gray-900 font-body">Flights</a>
-            </Link>
-            <Link href="/" passHref>
-              <a className="text-center text-gray-900 font-body">Bookings</a>
-            </Link>
-            <Link href="/" passHref>
-              <a className="text-center text-gray-900 font-body">Login</a>
-            </Link>
+            <NavItem title="Destination" />
+            <NavItem title="Hotels" />
+            <NavItem title="Flights" />
+            <NavItem title="Bookings" />
+            <NavItem title="Login" />
             <Link href="/" passHref>
               <a className="px-4 py-3 text-sm text-gray-900 border border-gray-900 rounded lg:rounded-lg font-body lg:text-base lg:px-6 lg:py-2 shadow-btnMd">
                 Sign Up
